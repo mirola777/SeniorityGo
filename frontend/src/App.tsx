f57801dfa30575be95881634936f44390e572832
jmmunoza <jmmunoza@eafit.edu.co>
2023-03-22 10:04:53 -0500

    ReactJS to ReactTS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
 
 function App() {
 	const [data, setData] = useState(null);
