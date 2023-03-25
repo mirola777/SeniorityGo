f65b9eb26177142612637a687a0be2513693c584
Mirola <jmmunoza@eafit.edu.co>
2023-03-25 10:33:56 -0500

    Cleaning App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,8 @@
-import { useState, useEffect } from 'react';
-import axios from "axios";
-import logo from './logo.svg';
 import './App.css';
-import {  Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import AdminRoutes from './routes/AdminRoutes';
 
 function App() {
-	
-	/*
-	const [data, setData] = useState<Profile[]>([]);
-
-	const apiUrl = process.env.REACT_APP_BACKEND_URL;
-	useEffect(() => {
-			axios.get(apiUrl +  'api/profile/all').then((response) => {
-				const profiles: Profile[] = response.data.map((json: any) => {
-					const profile = new Profile(
-						json.id,
-						json.name,
-						json.description,
-						[],
-						[]
-					);
-
-					return profile;
-				});
-
-				setData(profiles);
-			});
-		})
-
-	*/
-	
-
 	return (
 		<div>
 			<Routes>
--- frontend/src/pages/admin/Home.tsx
+++ frontend/src/pages/admin/Home.tsx
@@ -1,4 +1,29 @@
-const Home = () => {
+function Home() {
+/*
+	const [data, setData] = useState<Profile[]>([]);
+
+	const apiUrl = process.env.REACT_APP_BACKEND_URL;
+	useEffect(() => {
+			axios.get(apiUrl +  'api/profile/all').then((response) => {
+				const profiles: Profile[] = response.data.map((json: any) => {
+					const profile = new Profile(
+						json.id,
+						json.name,
+						json.description,
+						[],
+						[]
+					);
+
+					return profile;
+				});
+
+				setData(profiles);
+			});
+		})
+
+	*/
+
+
     return (
         <h1 className="text-4xl font-bold underline">
             Helloa world!
