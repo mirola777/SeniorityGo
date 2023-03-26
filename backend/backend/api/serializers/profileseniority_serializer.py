ea061bd18dede04de161df4c296c7c273baaaea8
Josem0117 <Drase0117@gmail.com>
2023-03-26 02:38:36 -0500

    changes for inputs and visualizing

--- backend/backend/api/serializers/profileseniority_serializer.py
+++ backend/backend/api/serializers/profileseniority_serializer.py
@@ -1,15 +1,29 @@
 from rest_framework import serializers
 from api.models.profileseniority import ProfileSeniority
-from api.serializers.seniority_serializer import SenioritySerializer
-from drf_writable_nested.serializers import WritableNestedModelSerializer
-
 from api.models.seniority import Seniority
 from api.models.pokemon import Pokemon
 from api.serializers.pokemon_serializer import PokemonSerializer
+from api.serializers.seniority_serializer import SenioritySerializer
 
+class CustomSeniorityPrimaryKeyRelatedField(serializers.PrimaryKeyRelatedField):
+    def to_representation(self, value):
+        # Fetch the actual Seniority instance
+        seniority = Seniority.objects.get(pk=value.pk)
+        serializer = SenioritySerializer(seniority)
+        return serializer.data
+
+class CustomPokemonPrimaryKeyRelatedField(serializers.PrimaryKeyRelatedField):
+    def to_representation(self, value):
+        # Fetch the actual Pokemon instance
+        pokemon = Pokemon.objects.get(pk=value.pk)
+        serializer = PokemonSerializer(pokemon)
+        return serializer.data
+    
 class ProfileSenioritySerializer(serializers.ModelSerializer):
-    seniority = SenioritySerializer()
-    pokemon = PokemonSerializer()
+    #seniority = serializers.PrimaryKeyRelatedField(queryset=Seniority.objects.all())
+    #pokemon = serializers.PrimaryKeyRelatedField(queryset=Pokemon.objects.all())
+    seniority = CustomSeniorityPrimaryKeyRelatedField(queryset=Seniority.objects.all())
+    pokemon = CustomPokemonPrimaryKeyRelatedField(queryset=Pokemon.objects.all())
 
     class Meta:
         model = ProfileSeniority
