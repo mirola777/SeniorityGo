b822f5bf5711df382c02433681b3fc1bc0833868
Josem0117 <Drase0117@gmail.com>
2023-03-26 02:28:08 -0500

    cleaning stuff

--- backend/backend/api/serializers/profile_serializer.py
+++ backend/backend/api/serializers/profile_serializer.py
@@ -1,10 +1,7 @@
-from rest_framework import serializers
+
 from api.models.profile import Profile
 from drf_writable_nested.serializers import WritableNestedModelSerializer
-from api.serializers.seniority_serializer import SenioritySerializer
 from api.serializers.profileseniority_serializer import ProfileSenioritySerializer
-from api.models.seniority import Seniority
-from api.models.profileseniority import ProfileSeniority
 
 class ProfileSerializer(WritableNestedModelSerializer):
     profileseniority_set = ProfileSenioritySerializer(many=True, required=False)
