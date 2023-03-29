b0ddc9bf81e8470a8ccc703d5b2be2236f9c87cb
Mirola <jmmunoza@eafit.edu.co>
2023-03-29 11:03:00 -0500

    Fixing creating and updating bugs

--- backend/backend/api/views/requirement_views.py
+++ backend/backend/api/views/requirement_views.py
@@ -5,26 +5,6 @@ from api.models.requirement import Requirement
 from api.serializers.requirement_serializer import RequirementSerializer
 
 
-def _checkNameIsUnique(serializer, requirement_id=-1):
-    """
-    Verifies that the requirement name is unique within the organization, excluding the given requirement ID.
-
-    Args:
-        serializer (Serializer): The serializer containing the data to be validated.
-        requirement_id (int, optional): The ID of the requirement object to exclude from the check. Defaults to -1.
-
-    Returns:
-        bool: True if the name is unique, False otherwise.
-    """
-    name = serializer.validated_data['name']
-    organization_id = serializer.validated_data['organization']
-
-    requirement_count = Requirement.objects.exclude(id=requirement_id).filter(
-        organization=organization_id, name=name).count()
-
-    return requirement_count == 0
-
-
 @api_view(['GET'])
 def getAll(request):
     requirements = Requirement.objects.all()
@@ -37,11 +17,6 @@ def create(request):
     serializer = RequirementSerializer(data=request.data)
 
     if serializer.is_valid():
-        if not _checkNameIsUnique(serializer):
-            return Response({
-                'error': 'Requirement name must be unique in the organization'
-            })
-
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -79,11 +54,6 @@ def update(request, pk):
 
     serializer = RequirementSerializer(requirement, data=request.data)
     if serializer.is_valid():
-        if not _checkNameIsUnique(serializer, requirement.id):
-            return Response({
-                'error': 'Requirement name must be unique in the organization'
-            })
-
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
