2b7fff6bdd38fa98d95b5be8cc29bfde41f037c4
Mirola <jmmunoza@eafit.edu.co>
2023-05-04 22:20:20 -0500

    User card!

--- frontend/src/components/admin/user/UserCard.tsx
+++ frontend/src/components/admin/user/UserCard.tsx
@@ -1,5 +1,3 @@
-import { ReactComponent as SeniorityIcon } from '../../../assests/icons/CodeBracket.svg';
-import { t } from 'i18next';
 import { Developer } from '../../../models/Developer';
 import { Admin } from '../../../models/Admin';
 
@@ -10,25 +8,31 @@ interface UserCardProps {
 
 function UserCard({ user }: UserCardProps) {
     return (
-        <div className="transition ease-in-out hover:scale-105 hover:bg-indigo-500 duration-150 rounded-2xl max-w-md bg-gradient-to-r from-fuchsia-700 to-blue-600 p-1 shadow-2xl">
-            <div className="transition block rounded-xl hover:bg-gray-800/50 duration-150 bg-gray-800/80 p-4 sm:p-6 lg:p-8">
-                <div className="my-4 space-y-4 text-center">
-                    <SeniorityIcon className="w-16 h-16 mx-auto text-gray-200" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
-                    <h3 className="text-lg font-bold text-gray-200 sm:text-xl">
-                        {user.getUser().getUsername()}
-                    </h3>
-                    <p className="mt-2 text-sm text-gray-400">
-                        {t('level') + " " + user.getUser().getEmail()}
-                    </p>
-                    {user instanceof Developer && (<img className="mx-auto rounded-full" src={user.getAvatar()} alt="avatar" />)}
-                    {user instanceof Developer && (user.getDeveloperProfiles().map((developerprofile) => {
-                            return <p className="mt-2 text-sm text-gray-400">
-                                {developerprofile.getProfile().getName() + " (" + developerprofile.getSeniority().getName() + ")"}
-                            </p>
-                        })
-                    )}
-         
+        <div className="transition ease-in-out hover:scale-105 hover:bg-indigo-500 duration-150 rounded-2xl  bg-gradient-to-r h-full from-fuchsia-700 to-blue-600 p-0.5 shadow-2xl">
+            <div className="transition block rounded-xl h-full hover:bg-gray-800/70 duration-150 bg-gray-800/90 p-4 space-y-4">
+                <div className="flex items-center gap-4">
+                    {user instanceof Developer && (<img className="h-14 w-14  object-cover rounded-full" src={user.getAvatar()} alt="avatar" />)}
+                    <div>
+                        {user instanceof Developer ?
+                            (<h3 className="text-base font-medium text-white">{user.getFirstName() + " " + user.getLastName()}<span className='text-gray-400'> ({user.getUser().getUsername()})</span></h3>) :
+                            (<h3 className="text-base font-medium text-white">{user.getUser().getUsername()}</h3>)}
+                        <h3 className="text-sm font-medium text-white"><span className='text-gray-400'>{user.getUser().getEmail()}</span></h3>
+                    </div>
                 </div>
+
+                {user instanceof Developer && user.getDeveloperProfiles().length > 0 && (
+                    <ul className="mt-4 space-y-2">
+                        {user instanceof Developer && (user.getDeveloperProfiles().map((developerprofile) => {
+                            return (
+                                <li>
+                                    <div className="block h-full text-base rounded-lg border-2 border-blue-600 px-4 py-1.5 hover:border-fuchsia-700">
+                                        <strong className="font-medium text-sm text-white">{developerprofile.getProfile().getName() + " (" + developerprofile.getSeniority().getName() + ")"}</strong>
+                                    </div>
+                                </li>
+                            )
+                        }))}
+                    </ul>
+                )}
             </div>
         </div>
     );
