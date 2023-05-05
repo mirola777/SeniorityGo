b8626f207d02dbfd01a953d48ef5ef62abcb2f51
Mirola <jmmunoza@eafit.edu.co>
2023-05-04 22:28:51 -0500

    fixing cards

--- frontend/src/components/common/ProfileCard.tsx
+++ frontend/src/components/common/ProfileCard.tsx
@@ -6,8 +6,8 @@ interface ProfileCardProps {
 
 function ProfileCard({ profile }: ProfileCardProps) {
     return (
-        <div className="transition ease-in-out hover:scale-105 hover:bg-indigo-500 duration-150 rounded-2xl max-w-md bg-gradient-to-r from-fuchsia-700 to-blue-600 p-1 shadow-2xl">
-            <a className="transition block rounded-xl hover:bg-gray-800/50 duration-150 bg-gray-800/80 p-4 sm:p-6 lg:p-8" href="#">
+        <div className="h-full transition ease-in-out hover:scale-105 hover:bg-indigo-500 duration-150 rounded-2xl max-w-md bg-gradient-to-r from-fuchsia-700 to-blue-600 p-1 shadow-2xl">
+            <a className="h-full transition block rounded-xl hover:bg-gray-800/50 duration-150 bg-gray-800/80 p-4 sm:p-6 lg:p-8" href="#">
                 <div className="my-24 text-center">
                     <h3 className="text-lg font-bold text-gray-200 sm:text-xl">
                         {profile.getName()}
